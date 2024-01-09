96c988e7cfa47ddf1decf45744cd588dfe02e49f
Agonpas <agonpas@gmail.com>
2024-01-09 19:54:21 +0100

    clase Tigger creada

---
+++ Nivel 1/Tigger.php
@@ -0,0 +1,10 @@
+<?php
+class Tigger {
+    private function __construct() {
+        echo "Building charactr..." . PHP_EOL;
+    }
+    public function roar(){
+        echo "Grrrr!" . PHP_EOL;
+    }
+}
+?>
