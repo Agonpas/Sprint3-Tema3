3f3f5a868234f992fe305e381df47ba8d80eb387
Agonpas <agonpas@gmail.com>
2024-01-10 12:35:28 +0100

    Añadimos un Tigger y lo hacemos rugir

--- Nivel 1/index.php
+++ Nivel 1/index.php
@@ -2,6 +2,7 @@
 include_once 'Tigger.php';
 // Uso del Singleton
 $tigger1 = Tigger::getInstancia();
+$tigger2 = Tigger::getInstancia();
 
 /*Llamamos al método roar unas cuantas veces*/
 $tigger1->roar();
@@ -9,6 +10,8 @@ $tigger1->roar();
 $tigger1->roar();
 $tigger1->roar();
 $tigger1->roar();
+$tigger2->roar();
+$tigger2->roar();
 
 // Obtener el número de rugidos
 echo "Número de rugidos: " . $tigger1->getRoarContador() . PHP_EOL;
